e97b21d42ac725079ff749b121479ec115d78d31
Adam <adam-cheaib@hotmail.com>
2023-04-21 14:06:02 +0200

    login_function attempt

--- login_register/index.js
+++ login_register/index.js
@@ -1,4 +1,19 @@
 "use strict"
 
 const input_section = document.getElementById("input_section");
-const submit_button = input_section.querySelector("form > button");
+const form = input_section.querySelector("form");
+
+form.addEventListener("click", tryToLogin);
+
+function tryToLogin(event) {
+    event.preventDefault();
+    const username_field = document.querySelector("form > input:nth-child(1)");
+    const password_field = document.querySelector("form > input:nth-child(2)");
+
+    fetch("./php/user_database.php", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ username: username_field.value, password: password_field.value })
+    })
+
+}
