2ec57290e682a580a4ddd2c89b7af9b1cb98cbe5
Adam <adam-cheaib@hotmail.com>
2023-04-21 16:32:15 +0200

    Login and registration almost DONE

--- login_register/php/user_database.php
+++ login_register/php/user_database.php
@@ -22,18 +22,38 @@ $action = $received_data["action"];
 $username = $received_data["username"];
 $password = $received_data["password"];
 
-if ($action = "register") {
-    $new_user = ["username" => $username, "password" => $password];
 
-
-}
-
-if ($action == "login") {   
-    if ($username == "" or $password == "") {
-        $message = ["Error" => "Invalid Credentials"];
-        sendJSON($message, 400);
+if ($action == "register") {
+    if ($users == null) {
+        $users = [];
     }
-        // CHECK THE JSON FILE IF THE USER EXISTS AND SEND BACK A SUCCESS MESSAGE
+
+    $id = 0;
+    if (1 < count($users)) {
+        $new_user = ["username" => $username, "password" => $password];
+        foreach ($users as  $single_user) {
+            if ($id < $single_user["id"]) {
+                $id = $single_user["id"];
+            }
+        }
+    }
+    $new_user["id"] = $id + 1;
+    $users[] = $new_user;
+    file_put_contents($filename, json_encode($users, JSON_PRETTY_PRINT));
+    $message = ["username" => $new_user["username"]];
+    sendJSON($message);
 }
 
+if ($action == "login") {
+    foreach ($users as $single_user) {
+        if($username == $single_user["username"] and $password == $single_user["password"]) {
+            $message = ["username" => $single_user["username"], "message" => "Login successful!"];
+            sendJSON($message);
+        }
+    } 
+
+    $message = ["Error" => "User not found"];
+    sendJSON($message, 404);
+
+}
 ?>
