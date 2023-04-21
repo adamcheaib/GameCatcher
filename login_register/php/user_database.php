428d632177bf70a795f6b47def57160b9b532569
Callw02 <muficcalle@gmail.com>
2023-04-21 14:54:30 +0200

    checks if user file exist otherwise creates

--- login_register/php/user_database.php
+++ login_register/php/user_database.php
@@ -2,6 +2,12 @@
 
 ini_set("display_errors", 1);
 
+$filname = "../../database/users.json";
+if(file_exists($filename)){
+    $data = file_get_contents($filename);
+    $users = json_decode($data, true);
+}
+
 function sendJSON($message, $http_code = 200) {
     header("content-type: application/json");
     http_response_code($http_code);
