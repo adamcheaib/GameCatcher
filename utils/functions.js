0ad32335295d5ccb7cc1a22117e192389c9544af
Abbe96 <adam-cheaib@hotmail.com>
2023-05-03 14:42:17 +0200

    minor changes

--- utils/functions.js
+++ utils/functions.js
@@ -1,13 +1,13 @@
 "use strict"
 
 function popUpFunction(message) {
-    button.disabled = true;
+    button.disabled = true; // Vad menar Calle med detta? <3
     console.log("hef");
-    body = document.querySelector("body");
+    const body = document.querySelector("body");
     document.querySelector("div").style.opacity = "0.5";
 
-    popup = document.createElement("div");
-    exit_button = document.createElement("div");
+    const popup = document.createElement("div");
+    const exit_button = document.createElement("div");
 
     body.appendChild(popup);
     popup.classList.add("popup");
@@ -21,24 +21,8 @@ function popUpFunction(message) {
 }
 function remove_message(event) {
     console.log(event);
-    div = event.originalTarget.parentElement;
+    const div = event.originalTarget.parentElement;
     div.remove();
     button.disabled = false;
     document.querySelector("div").style.opacity = "1";
 }
-
-// Fetch games based on the genre!
-
-function TEST_fetch_games(count) { // Fetches games based on genre!
-    let link = prefix + "games?&key=a25ef91c11654298888f4907971ad496";
-    fetch(link).then(r => r.json()).then(game => {
-        console.log(game);
-        game.results.forEach(desc => {
-            desc.genres.forEach(gameNew => {
-                if (gameNew.name == "Adventure") { // Choose the genre that you want to search for!
-                    console.log(desc.name);
-                }
-            })
-        })
-    });
-};
