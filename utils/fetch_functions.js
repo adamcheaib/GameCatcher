aa5347706e2e01cad5dd73dc83969e2bbeba2bf4
Abbe96 <adam-cheaib@hotmail.com>
2023-05-03 15:19:38 +0200

    adjustments

--- utils/fetch_functions.js
+++ utils/fetch_functions.js
@@ -7,13 +7,8 @@ fetch_button.addEventListener("click", TEST_fetch_all_games)
 // https://api.rawg.io/api/platforms?key=a25ef91c11654298888f4907971ad496
 const api_key = "&key=a25ef91c11654298888f4907971ad496";
 let prefix = "https://api.rawg.io/api/";
-let page = 1;
 
-if (page == 1) {
-    document.getElementById("previous").disabled = true;
-}
-
-async function TEST_fetch_all_games() { // Fetches games based on genre!
+async function TEST_fetch_all_games(page = 1) { // Fetches games based on genre!
     let proper_prefix = prefix + `games?page=${page}${api_key}`;
     const link = new Request(proper_prefix);
 
@@ -27,7 +22,6 @@ async function TEST_fetch_all_games() { // Fetches games based on genre!
 };
 
 async function TEST_fetch_genre_games(event) {
-    const checkbox = document.getElementById("horror").name; // Add the element thingy where the name of the genre is generated from!
 
     async function fetch_specific_genre(genre) {
         try {
@@ -39,45 +33,4 @@ async function TEST_fetch_genre_games(event) {
         }
     }
 
-    fetch_specific_genre(checkbox);
-}
-
-
-document.getElementById("fetch").addEventListener("click", TEST_fetch_genre_games);
-document.getElementById("next").addEventListener("click", next_page);
-document.getElementById("previous").addEventListener("click", previous_page);
-
-
-
-function append_fetch_result(games_array) {
-    games_array.forEach(game => {
-        const game_image = document.createElement("img");
-        const parent = document.getElementById("game_preview");
-        const image_link = game.short_screenshots[1].image;
-        game_image.src = image_link;
-        parent.appendChild(game_image);
-    })
-
-}
-
-
-// Test
-function next_page() {
-    page++;
-    if (page >= 1) {
-        document.getElementById("previous").disabled = false;
-    }
-    TEST_fetch_all_games();
-}
-
-function previous_page(event) {
-    if (page >= 1) {
-        page--;
-        event.target.disabled = true;
-        TEST_fetch_all_games();
-    } else {
-        event.target.disabled = false;
-        page--;
-        TEST_fetch_all_games();
-    }
 }
