afdb8e152ad207162cba173fa6eef5996afc2976
adamcheaib <adam-cheaib@hotmail.com>
2023-05-11 12:26:31 +0200

    Current logged on user saved
    When navigating to the frontpage, the username that is logged on is saved

--- frontpage/js/front_page.js
+++ frontpage/js/front_page.js
@@ -1,8 +1,24 @@
 import { init_forum } from "./forum.js";
 import { init_collection } from "./game_collection.js";
 import { genre_scroll } from "../../utils/functions.js";
-localStorage.setItem("hello", "hello");
+
+async function get_logged_on_user() {
+    const response = await fetch("../../login_register/php/logged_on_user.php");
+    const resource = await response.json();
+    console.log(resource);
+
+    if (!response.ok) {
+        window.location.replace("http://localhost:1234/login_register");
+    } else {
+        window.localStorage.setItem("username", resource.logged_on_user);
+        init_frontpage();
+    }
+};
+
+get_logged_on_user();
+
 export function init_frontpage() {
+    document.body.innerHTML = "";
 
     document.querySelector("link").setAttribute("href", "./frontpage/css/frontpage.css");
     document.body.innerHTML = `
@@ -20,7 +36,7 @@ export function init_frontpage() {
 
                 <div id="profile_and_welcome_text">
                     <div id="welcome_text">
-                        <h1>Hello Elliot</h1>
+                        <h1>Hello ${window.localStorage.getItem("username")}!</h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing</p>
                     </div>
 
@@ -75,6 +91,6 @@ export function init_frontpage() {
     `;
     genre_scroll();
     document.querySelector("#saved").addEventListener("click", init_collection);
-    document.querySelector("#main_page").addEventListener("click", init_frontpage);
+    document.querySelector("#main_page").addEventListener("click", get_logged_on_user);
     document.querySelector("#chat").addEventListener("click", init_forum)
 }
