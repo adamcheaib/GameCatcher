60d11f53533f6c4dee8e9fb9b6f4070d3a66de42
itsDipsy <elliot.collinszcv@gmail.com>
2023-05-17 15:16:02 +0200

    uppdate

--- frontpage/js/find_friends.js
+++ frontpage/js/find_friends.js
@@ -26,7 +26,12 @@ import { init_collection } from "./game_collection.js";
 //     console.log("User not found :(");
 // }
 
+
+
+
 export async function init_friends_page() {
+
+
 
     if (document.querySelector(".display_game_dom") !== null) {
         document.querySelector(".display_game_dom").remove();
@@ -34,9 +39,27 @@ export async function init_friends_page() {
     document.querySelector("link").setAttribute("href", "./frontpage/css/friends_page.css");
 
     document.querySelector("#center_piece").innerHTML = `
-
+        <div id="navigation">
+            <div class="selected" ">Add Friends</div>
+            <div class="unselected" >Pending</div>
+            <div class="unselected" >Blocked</div>
+        </div>
+        <div id="search_wrapper">
+            <input id="search"></input>
+            <div id="search_image"></div>
+        </div>
+        <div id="display"></div>
     
     `;
+
+    document.querySelector("#search_image").addEventListener("click", find_friend);
+}
+
+async function find_friend() {
+    let account_name = document.querySelector("input").value;
+    let response = await fetch(`./frontpage/php/find_friend.php?account_name=${account_name}`);
+    let account_data = await response.json();
+    console.log(account_data);
 }
 
 document.querySelector("#main_page").addEventListener("click", init_frontpage);
