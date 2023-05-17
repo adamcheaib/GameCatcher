a4201aa027a24ed496f98b0a1ed4d7cab01a7798
itsDipsy <elliot.collinszcv@gmail.com>
2023-05-17 14:21:40 +0200

    uppdate

--- frontpage/js/find_friends.js
+++ frontpage/js/find_friends.js
@@ -1,22 +1,22 @@
-document.addEventListener("keydown", get_users);
+// document.addEventListener("keydown", get_users);
 
 
 
-function get_users(event){
-    if(event.key === "Enter"){
-        console.log("Rätt");
-        fetch("../../database/users.json")
-        .then(r => r.json()).then(resource => find_friend(resource));
-    }
+// function get_users(event){
+//     if(event.key === "Enter"){
+//         console.log("Rätt");
+//         fetch("../../database/users.json")
+//         .then(r => r.json()).then(resource => find_friend(resource));
+//     }
     
-}
+// }
 
-function find_friend(usersArray){
-    let username = document.querySelector("#username").value;
-    for(let i = 0; i < usersArray.length;i++){
-        if(usersArray[i].username === username){
-            console.log(usersArray[i]);
-        }
-    }
-    console.log("User not found :(");
-}
+// function find_friend(usersArray){
+//     let username = document.querySelector("#username").value;
+//     for(let i = 0; i < usersArray.length;i++){
+//         if(usersArray[i].username === username){
+//             console.log(usersArray[i]);
+//         }
+//     }
+//     console.log("User not found :(");
+// }
