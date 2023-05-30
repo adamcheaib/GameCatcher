86a545472add542c530b4463ab5e541d88df0a56
Adam <adam-cheaib@hotmail.com>
2023-05-30 15:26:02 +0200

    Kommentarer på profile.js done!

--- frontpage/profile/js/index.js
+++ frontpage/profile/js/index.js
@@ -3,13 +3,13 @@ let banner_picture = document.querySelector("#banner_picture");
 let profile_picture = document.querySelector("#profile_pic_form");
 let favorite_game = document.querySelector("#upload_favorite_game");
 
-console.log(window.location.search);
+// Plockar ut användarnamnet man försöker fetcha profilen på. Det gör vi genom att plocka ut användarnamnet från URL:n.
 const profile_to_fetch = window.location.search.split("?username=")[1];
-console.log(profile_to_fetch);
+
+// Kör funktonen med användarnamnet som argument.
 get_all_users(profile_to_fetch);
 
 async function get_all_users(username) {
-
     try {
         const response = await fetch(`./php/profiles.php?username=${username}`);
         const user = await response.json();
@@ -27,7 +27,12 @@ async function get_all_users(username) {
 }
 
 async function go_to_own_profile(user) {
-    console.log(user);
+
+    /*
+    Visar den inloggade användarens sida tillsammans med alla sina profilbilder, banner_picture, och status meddelanden.
+    Här behålls input-elementen så att man kan ändra sina bilder osv.
+    */
+
     if (user.username === localStorage.username) {
         document.querySelector("h2").textContent = user.username;
         if (user.profile_picture == "undefined") {
@@ -64,7 +69,8 @@ async function go_to_own_profile(user) {
 
 
 function other_users_profiles(other_user) {
-    console.log(other_user);
+    // Tar bort alla input-elementen så att man ej kan ändra bilderna och visar användarens profil_bild, banner_bild, status_meddelanden.
+    // Med andra ord, så visas en användare som INTE är den som är inloggad!
     document.getElementById("upload_banner_picture").remove();
     document.getElementById("upload_profile_picture").remove();
     document.getElementById("upload_favorite").remove();
@@ -95,22 +101,27 @@ function other_users_profiles(other_user) {
 }
 
 
+// Funktionen för att kunna ladda upp olika bilder (profil_bild och banner_bild).
 function upload_picture(event) {
     event.preventDefault();
     let image;
     let formData;
     let check_profile_pic = false;
+
+    // Kontrollerar event.targets ID för att identifiera vad det är den ska skicka till PHP-filen och vilken bild det är som ska skickas och vart den ska visas.
     if (event.target.id === "upload_profile_picture") {
         image = document.querySelector("#profile_image");
         formData = new FormData(profile_picture);
         formData.append("action", "profile_picture");
         check_profile_pic = true;
     }
+
     if (event.target.id === "upload_banner_picture") {
         image = document.querySelector("main");
         formData = new FormData(banner_picture);
         formData.append("action", "banner_picture");
     }
+
     if (event.target.id === "upload_favorite") {
         image = document.querySelector("#favorite_game_image");
         formData = new FormData(favorite_game);
@@ -123,17 +134,17 @@ function upload_picture(event) {
         body: formData,
     });
 
+    // Här skickas det man laddar upp och det som skickas beror på event.targets ID.
     fetch(request)
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             image.style.backgroundImage = `url(./images/${data.filename})`;
 
+            // if-satsen finns för att profilbilderna för varje kommentar ska OCKSÅ förändras!
             if (check_profile_pic === true) {
                 localStorage.setItem("profile_picture", data.filename);
                 document.getElementById("comment_profile").style.backgroundImage = `url(./images/${data.filename})`;
                 document.querySelectorAll(".profile_picture").forEach(element => {
-                    console.log(element);
                     element.style.backgroundImage = `url(./images/${localStorage.getItem("profile_picture")})`;
                 })
             }
@@ -146,7 +157,8 @@ function send_message(event) {
     let section = document.querySelector("#profile_forum")
     let message = document.querySelector("#message").value
     if (message.length > 25) {
-        alert("Message is too long, it needs to be shorter than 25 characters")
+        alert("Message is too long, it needs to be shorter than 25 characters");
+        // Return finns för att den ska stoppa funktionen!
         return;
     }
 
@@ -163,9 +175,11 @@ function send_message(event) {
 
     });
 
+
     fetch(request)
         .then(resource => resource.json())
         .then(data => {
+            // Skapar containern för varje status_meddelande.
             div.innerHTML = `
                 <div id="chat_comments">
                 <div class="profile_picture" style='background-image: url("./images/${localStorage.getItem("profile_picture")}'></div>
@@ -183,6 +197,7 @@ function send_message(event) {
     section.appendChild(div)
     document.querySelector("#message").value = ""
 }
+
 
 function show_messages(messages, { profile_picture }) {
     console.log(messages);
@@ -205,8 +220,10 @@ function show_messages(messages, { profile_picture }) {
                     <div class="delete">delete</div>
                     </div>
             
-            `
-            section.appendChild(div)
+            `;
+            // Appendar meddelandet till meddelanden på sidan.
+            section.appendChild(div);
+            // Ger varje status_meddelande en delete knapp som tar bort kommentaren.
             let deleteButtons = document.querySelectorAll(".delete");
             deleteButtons.forEach(button => {
                 button.addEventListener("click", remove_comment);
@@ -215,6 +232,7 @@ function show_messages(messages, { profile_picture }) {
     }
 }
 
+// Tar bort status meddelanden.
 function remove_comment(event) {
     let timestamp = event.target.parentElement.querySelector("#timestamp").innerHTML;
     let request = new Request("./php/upload.php", {
@@ -232,6 +250,8 @@ function remove_comment(event) {
             console.log(data)
             event.target.parentElement.remove();
         })
+
+    // Tar bort alla (denna bör inte vara med).
     document.querySelectorAll(".comments_section").forEach(element => element.remove());
 
     fetch("../../database/users.json")
@@ -248,6 +268,7 @@ function remove_comment(event) {
 
 }
 
+// Lägger till eventListener på #go_home knappen så att man kan gå tillbaka till framsidan.
 document.querySelector("#go_home").addEventListener("click", go_home)
 function go_home(event) {
     window.location.replace("../../frontpage");
